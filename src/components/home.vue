aa11bad9e9808d26b6f41cded94008ce1ac2023f
zhangwenbo <zhangwenbo@digsur.com>
2020-02-17 10:48:21 +0800

    ''头部布局''

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,31 +1,34 @@
 <template>
-    <div  style="width: 100%">
-      <!--<v-header></v-header>-->
-      <div class="nav">
+  <div style="width: 100%">
+    <!--<v-header></v-header>-->
+    <div class="nav">
       <v-nav></v-nav>
-      </div>
-      <div class="content">
-        <router-view></router-view>
-      </div>
     </div>
+    <div class="content">
+      <!-- <router-view></router-view> -->
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-  import header from '../components/header.vue'
-  import nav from '../components/nav.vue'
-    export default{
-        data(){
-            return {}
-        },
-        components: { 'v-header': header,
-          'v-nav': nav}
-    }
+import header from '../components/header.vue'
+import nav from '../components/nav.vue'
+export default {
+  data () {
+    return {}
+  },
+  components: {   
+    'v-header': header,
+    'v-nav': nav  
+  }
+}
 </script>
 <style scoped>
-  .nav{
-    box-shadow: 0 2px 4px 0 rgba(70, 76, 91, .52), 0 0 6px 0 rgba(70, 76, 91, .04);
-    height: 80px;
-    background: #ebebeb;
-  }
-   .content{
-   }
+.nav {
+  box-shadow: 0 2px 4px 0 rgba(70, 76, 91, 0.52),
+    0 0 6px 0 rgba(70, 76, 91, 0.04);
+  height: 80px;
+  background: #ebebeb;
+}
+.content {
+}
 </style>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,145 +1,232 @@
 <template>
-  <div class="navWrapper"  >
+  <div class="navWrapper">
     <div class="logo">
-
-      <span class="shengdian">
+      <span>
         管理系统
       </span>
     </div>
-    <div style="margin: 0 auto ;width: 70%">
-  <Menu mode="horizontal" class="navMainWrapper" theme="dark" :active-name="setActive"  :default-active="onRoutes" @on-select="routeTo">
 
-   <!-- <Menu-item name="identify">
-      <Icon type="ios-people"></Icon>
-      人员认证
-    </Menu-item>-->
-   <Menu-item name="pickUp">
-      <Icon type="ios-people"></Icon>
-     第一个
-    </Menu-item>
-    <Menu-item name="acquisition">
-      <Icon type="ios-paper"></Icon>
-      第二个
-    </Menu-item>
-    <Menu-item name="analyse">
-      <Icon type="ios-people"></Icon>
-      地上那个
-    </Menu-item>
-    <Menu-item name="manage" >
-      <Icon type="ios-people"></Icon>
-      地下哪个
-    </Menu-item>
+    <div class="control">
+      <div class="router">
+        <div class="refresh">
+          <Icon type="refresh" />
+        </div>
+        <div>
+          <span>1</span>/<span>2</span>/<span>3</span>
+        </div>
+      </div>
 
+      <div class="option">
+        <input type="text" placeholder="搜索...">
+        <div class="icon">
+          <Icon type="record"/>
+        </div>
+        <div class="icon">
+          <Icon type="full-screen" />
+        </div>
+        <div class="icon">
+          <Icon type="ios-notifications-outline" />
+        </div>
 
-  </Menu>
+        <Dropdown>
+          <div class="user">
+            <a href="javascript:void(0)">
+              <Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" />
+            </a>
+            <span>
+              admin
+              <!-- {{`${user.name}`}} -->
+            </span>
+          </div>
+
+          <DropdownMenu slot="list">
+            <DropdownItem v-for="(item,index) in adminOption" :key="index">
+              <Icon :type="item.icon" />
+              <span>{{item.name}}</span>
+            </DropdownItem>
+            <DropdownItem divided>
+              <Icon type="ios-log-out" />
+              <span>退出登录</span>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        <div class="icon">
+          <Icon type="md-globe" />
+        </div>
+      </div>
     </div>
-    <div style="line-height:60px;float: right">
 
-      <Dropdown trigger="click"   style="margin-right: 20px">
-        <a href="javascript:void(0)" style="color: #fff">
-          账户管理
-          <Icon type="arrow-down-b"></Icon>
-        </a>
-        <Dropdown-menu slot="list">
-          <Dropdown-item><p @click="showOut=true">退出</p></Dropdown-item>
-
-        </Dropdown-menu>
-      </Dropdown>
-
-    </div>
-    <Modal
-      v-model="showOut"
-      title="派大星系统"
-      @on-ok="ok"
-      @on-cancel="cancel">
+    <!-- <Modal v-model="showOut" title="派大星系统" @on-ok="ok" @on-cancel="cancel">
       <p>确定退出?</p>
-
-    </Modal>
+    </Modal> -->
   </div>
 </template>
 <script type="text/ecmascript-6">
 
-    export default{
-        data(){
-            return {
-              showOut:false,
-              visible: false
-            }
-        },
-      methods:{
-        routeTo(e) {
-          //console.log(e);
-          this.$router.push(e);
-        },
-        handleOpen () {
-          this.visible = true;
-        },
-        handleClose () {
-          this.visible = false;
-        },
-        ok(){
+export default {
+  data () {
+    return {
+      showOut: false,
+      visible: false,
+      adminOption: [
+        { name: '个人中心', icon: 'ios-contact-outline' },
+        { name: '设置', icon: 'ios-settings-outline' },
+      ]
+    }
+  },
+  methods: {
+    routeTo (e) {
+      //console.log(e);
+      this.$router.push(e);
+    },
+    handleOpen () {
+      this.visible = true;
+    },
+    handleClose () {
+      this.visible = false;
+    },
+    ok () {
+      this.$router.push({ path: '/login' })
+    }
+  },
+  components: {},
+  computed: {
+    onRoutes () {
+      return this.$route.path.replace('/', '');
+    },
+    setActive () {
+      return this.$route.path.replace('/', '');
+    }
+  }
+}
+</script>
 
-          this.$router.push({ path: '/login' })
-        }
-      },
-        components: {},
-      computed:{
-        onRoutes(){
-          return this.$route.path.replace('/','');
-        },
-        setActive() {
-          return this.$route.path.replace('/','');
+<style scoped lang="less">
+.navWrapper {
+  display: flex;
+  height: 80px;
+  width: 100%;
+  background: #fff;
+  box-shadow: 0 4px 10px 0 rgba(70, 76, 91, 0.52),
+    0 0 6px 0 rgba(70, 76, 91, 0.04);
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 15%;
+    height: 100%;
+    background: #1c2438;
+
+    span {
+      font-size: 32px;
+      font-weight: bold;
+      color: #fff;
+    }
+  }
+  .control {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 85%;
+    height: 100%;
+    padding: 0 1%;
+    .router {
+      display: flex;
+      align-items: center;
+      height: 100%;
+      .refresh {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 50px;
+        height: 100%;
+        margin-right: 8px;
+        cursor: pointer;
+      }
+      .refresh:hover {
+        background: rgba(0, 0, 0, 0.1);
+      }
+      div {
+        font-size: 20px;
+        color: #ccc;
+        span {
+          margin: 0 10px;
         }
       }
     }
-</script>
-<style scoped>
-  .navMainWrapper{
-     box-shadow: 0 0px 0px 0 rgba(70, 76, 91,.04) !important;
-  }
-  .navWrapper{
-    box-shadow: 0 4px 10px 0 rgba(70, 76, 91, .52), 0 0 6px 0 rgba(70, 76, 91,.04);
-    height: 70px;background: #1c2438;width: 100%; display: flex
-  }
-  .logo{
-    float: left;
-    line-height: 60px;
-    margin-left: 20px;
-    margin-top: 10px;
-    display: table;
-  }
-  .logo img{
-
-  }
-  .shengdian {
-    color: #fff;
-    font-size: 30px;
-    position: relative;
-    top: -10px;
-  }
-  .ivu-menu{
-    display: flex;
-    box-shadow: 0 2px 4px 0 rgba(70, 76, 91, .52), 0 0 6px 0 rgba(70, 76, 91,.04)
-  }
-    .ivu-menu-item{
-      flex: 1;
-      text-align: center;
+    .option {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      height: 100%;
+      input {
+        width: 80%;
+        height: 100%;
+        font-size: 22px;
+        padding-left: 5px;
+        outline: none;
+        border: none;
+      }
+      input::-webkit-input-placeholder {
+        color: #aab2bd;
+        font-size: 22px;
+      }
+      .icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50px;
+        height: 100%;
+        cursor: pointer;
+      }
+      .icon:hover {
+        background: rgba(0, 0, 0, 0.1);
+      }
+      .user {
+        display: flex;
+        align-items: center;
+        padding: 0 10px;
+        height: 100%;
+        span {
+          font-size: 18px;
+          margin-left: 8px;
+        }
+      }
     }
-  .ivu-menu-item:hover{
-    font-size: 20px;
+  }
+}
+.ivu-dropdown {
+  display: flex;
+  height: 100%;
+  .ivu-dropdown-rel {
+    height: 100%;
+  }
+}
 
-  }
-  .ivu-menu-item a{
-    color: #fff;
-  }
-  .ivu-menu-item-selected a{
-    font-size: 20px;
-  }
-  a:hover{
-    color: #fff !important;
-  }
+.ivu-menu {
+  display: flex;
+  box-shadow: 0 2px 4px 0 rgba(70, 76, 91, 0.52),
+    0 0 6px 0 rgba(70, 76, 91, 0.04);
+}
+.ivu-menu-item {
+  flex: 1;
+  text-align: center;
+}
+.ivu-menu-item:hover {
+  font-size: 20px;
+}
+.ivu-menu-item a {
+  color: #fff;
+}
+.ivu-menu-item-selected a {
+  font-size: 20px;
+}
+a:hover {
+  color: #fff !important;
+}
 
-  .ivu-menu-light {
-    background: #00d1b2;
-  }
+.ivu-menu-light {
+  background: #00d1b2;
+}
 </style>
