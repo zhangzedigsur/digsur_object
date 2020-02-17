85b056a4cf744f9e53a8be6518ecb0f13c3edda4
zhangwenbo <zhangwenbo@digsur.com>
2020-02-17 16:31:26 +0800

    '头部布局'

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -9,7 +9,7 @@
     <div class="control">
       <div class="router">
         <div class="refresh">
-          <Icon type="refresh" />
+          <Icon type="md-refresh" size='24'/>
         </div>
         <div>
           <span>1</span>/<span>2</span>/<span>3</span>
@@ -19,13 +19,13 @@
       <div class="option">
         <input type="text" placeholder="搜索...">
         <div class="icon">
-          <Icon type="record"/>
+          <Icon type="md-radio-button-on" size='24'/>
         </div>
         <div class="icon">
-          <Icon type="full-screen" />
+          <Icon type="ios-qr-scanner" size='24'/>
         </div>
         <div class="icon">
-          <Icon type="ios-notifications-outline" />
+          <Icon type="ios-notifications-outline" size='24'/>
         </div>
 
         <Dropdown>
@@ -41,25 +41,25 @@
 
           <DropdownMenu slot="list">
             <DropdownItem v-for="(item,index) in adminOption" :key="index">
-              <Icon :type="item.icon" />
+              <Icon :type="item.icon" size='24'/>
               <span>{{item.name}}</span>
             </DropdownItem>
             <DropdownItem divided>
-              <Icon type="ios-log-out" />
+              <Icon type="ios-log-out" size='24'/>
               <span>退出登录</span>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
 
         <div class="icon">
-          <Icon type="md-globe" />
+          <Icon type="md-globe" size='24'/>
         </div>
       </div>
     </div>
 
-    <!-- <Modal v-model="showOut" title="派大星系统" @on-ok="ok" @on-cancel="cancel">
+    <Modal v-model="showOut" title="提示" >
       <p>确定退出?</p>
-    </Modal> -->
+    </Modal>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -142,6 +142,7 @@ export default {
         width: 50px;
         height: 100%;
         margin-right: 8px;
+        padding: 0 15px;
         cursor: pointer;
       }
       .refresh:hover {
@@ -178,6 +179,7 @@ export default {
         align-items: center;
         width: 50px;
         height: 100%;
+        padding: 0 15px;
         cursor: pointer;
       }
       .icon:hover {
@@ -186,7 +188,7 @@ export default {
       .user {
         display: flex;
         align-items: center;
-        padding: 0 10px;
+        padding: 0 15px;
         height: 100%;
         span {
           font-size: 18px;
@@ -202,31 +204,43 @@ export default {
   .ivu-dropdown-rel {
     height: 100%;
   }
+  .ivu-dropdown-menu{
+    .ivu-dropdown-item .ivu-dropdown-item-divided{
+      display: flex;
+      align-items: center;
+      padding: 15px 10px;
+      span{
+        font-size: 16px;
+        margin-left: 5px;
+      }
+      
+    }
+  }
 }
 
-.ivu-menu {
-  display: flex;
-  box-shadow: 0 2px 4px 0 rgba(70, 76, 91, 0.52),
-    0 0 6px 0 rgba(70, 76, 91, 0.04);
-}
-.ivu-menu-item {
-  flex: 1;
-  text-align: center;
-}
-.ivu-menu-item:hover {
-  font-size: 20px;
-}
-.ivu-menu-item a {
-  color: #fff;
-}
-.ivu-menu-item-selected a {
-  font-size: 20px;
-}
-a:hover {
-  color: #fff !important;
-}
+// .ivu-menu {
+//   display: flex;
+//   box-shadow: 0 2px 4px 0 rgba(70, 76, 91, 0.52),
+//     0 0 6px 0 rgba(70, 76, 91, 0.04);
+// }
+// .ivu-menu-item {
+//   flex: 1;
+//   text-align: center;
+// }
+// .ivu-menu-item:hover {
+//   font-size: 20px;
+// }
+// .ivu-menu-item a {
+//   color: #fff;
+// }
+// .ivu-menu-item-selected a {
+//   font-size: 20px;
+// }
+// a:hover {
+//   color: #fff !important;
+// }
 
-.ivu-menu-light {
-  background: #00d1b2;
-}
+// .ivu-menu-light {
+//   background: #00d1b2;
+// }
 </style>
